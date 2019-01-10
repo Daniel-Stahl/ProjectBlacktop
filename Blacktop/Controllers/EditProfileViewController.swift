db704761deb6b958bcc3e0eaee0cb575b70a99d4
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-01-10 08:44:43 -0800

    Created navigation to profile

---
+++ Blacktop/Controllers/EditProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileViewController.swift
+//  Blacktop
+//
+//  Created by Daniel Stahl on 1/10/19.
+//  Copyright © 2019 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
